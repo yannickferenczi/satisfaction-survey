d850f0f14aee3d82a5d635db4590630916a70bea
yannickferenczi <>
2023-05-17 15:33:34 +0000

    Add requirements for deployment

--- run.py
+++ run.py
@@ -2,6 +2,8 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
+# -------------------- THIS REPRESENTS 80 CHARACTERS ------------------------ #
+
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -13,3 +15,6 @@ SCOPES = [
 CREDENTIALS = Credentials.from_service_account_file('creds.json', scopes=SCOPES)
 GSPREAD_CLIENT = gspread.authorize(CREDENTIALS)
 SPREADSHEET = GSPREAD_CLIENT.open('Cantina Satisfaction Survey')
+
+print("hello world!")
+print(SPREADSHEET.worksheet("Survey results").acell(f"B5").value)
