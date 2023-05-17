eb4dd084e45ddba8d017238535486d5d57ac9f21
yannickferenczi <>
2023-05-17 14:49:07 +0000

    Link run.py to the google spreadsheet

--- run.py
+++ run.py
@@ -2,3 +2,14 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPES = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive"
+]
+
+CREDENTIALS = Credentials.from_service_account_file('creds.json', scopes=SCOPES)
+GSPREAD_CLIENT = gspread.authorize(CREDENTIALS)
+SPREADSHEET = GSPREAD_CLIENT.open('Cantina Satisfaction Survey')
